276a56477290603b5f6abf576b5a229791538746
Evan Taylor <77093543+Evan031@users.noreply.github.com>
2022-03-09 17:26:44 +0200

    section 2: lecture 4

---
+++ oopsandbox/2_3.php
@@ -0,0 +1,33 @@
+<?php
+
+// Define a class
+class User
+{
+    // Properties (attributes or variables)
+    public $name;
+
+    // Methods (functions)
+
+    // below is an access modifier it can be public, private or protected
+    public function sayHello()
+    {
+        return $this->name . ' says Hello';
+    }
+}
+
+
+// Instantiate a user object from the user class
+$user1 = new User();
+
+$user1->name = 'Warren';
+echo $user1->name;
+echo '<br>';
+echo $user1->sayHello();
+
+echo '<br>';
+// Create new user
+$user2 = new User();
+$user2->name = 'Evan';
+echo $user2->name;
+echo '<br>';
+echo $user2->sayHello();
