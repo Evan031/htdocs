58e55fc43e1db56dfaaab540609415866e6af2b6
Evan Taylor <77093543+Evan031@users.noreply.github.com>
2022-05-03 17:05:12 +0200

    section 5: lecture 1

--- pdotest/index.php
+++ pdotest/index.php
@@ -12,17 +12,6 @@ $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname . ';port=' . $port;
 $pdo = new PDO($dsn, $user, $password);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-// $status = 'guest';
-
-// $sql = 'SELECT * FROM users WHERE status = :status';
-// $stmt = $pdo->prepare($sql);
-// $stmt->execute(['status' => $status]);
-// $users = $stmt->fetchAll();
-
-// foreach($users as $user){
-//     echo $user->name.'<br>';
-// }
-
 $name = 'Stacey Taylor';
 $email = 'stacey@gmail.com';
 $status = 'guest';
--- taylormvc/app/controllers/Pages.php
+++ taylormvc/app/controllers/Pages.php
@@ -3,13 +3,14 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        $this->postModel = $this->model('Post');
+        
     }
 
     public function index()
     {
+
         $data = [
-            'title' => 'Welcome'
+            'title' => 'Taylor MVC',
         ];
 
         $this->view('pages/index', $data);
